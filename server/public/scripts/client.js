ced17e705b68fefcaaec0f27463ca64e1f5f6592
Dev Jana <devjana@Devs-MacBook-Pro-2.local>
2020-02-07 14:54:43 -0600

    input capture

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,19 @@
 $( document ).ready( onReady );
 
+function addItem(){
+    console.log( 'in addItem' );
+    // get user input, place into an obj
+    let objectToSend = {
+        description: $( '#descriptionIn' ).val(),
+        color: $( '#colorIn' ).val(),
+        size: $( '#sizeIn' ).val()
+    }
+    console.log( 'sending:', objectToSend );
+    // make AJAX POST of obj
+    // update DOM
+    // catch any errors
+} // end addItem
+
 function onReady(){
-    console.log( 'jq' );
+    $( '#addItemButton' ).on( 'click', addItem );
 } // end onReady
